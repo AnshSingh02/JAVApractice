3d886342b777ce39d5dbd3f4d9f9a72511a92d94
Ansh Singh <ansh04072002@gmail.com>
2022-11-28 15:27:45 +0530

    sum

---
+++ conditional loops + calculator/public class Calculator {.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public static void main(String[]args) {
+        Scanner in =new Scanner (System.in);
+        int ans = 0;
+        while (true) {
+            //take the operator as input
+            char op = in.next().trim().charAt(0);
+            if (op == '+' || op =='-' || op == '*' || op == '%' || op == '/'){
+                //input two numbers 
+                int num1 =in.nextInt();
+                int num2 =in.nextInt();
+                if (op == '+'){
+                    ans =num1 +num2;
+                }
+                if (op == '-'){
+                    ans =num1 -num2;
+    
+                }
+                if (op == '*'){
+                    ans =num1 *num2;
+        } if (op == '%'){
+            ans =num1 % num2;
+    }if (op == '/'){
+        ans =num1 / num2;
+}
+            }}
+            System.op.println(ans);
+        }}
