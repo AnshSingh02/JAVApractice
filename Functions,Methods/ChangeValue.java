ce3a3132f82664a87fd7f274a1d25e8042769fae
Ansh Singh <ansh04072002@gmail.com>
2022-12-02 15:43:27 +0530

    changevalue

---
+++ Functions,Methods/ChangeValue.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class ChangeValue{
+    public static void main(String[] args) {
+        int[] arr ={1,3,2,45,6};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change (int[] nums){
+
+        nums[0] = 99; // if  u make a change to the obj via this ref variable , same obj will  be changed         
+    }
+
+}
