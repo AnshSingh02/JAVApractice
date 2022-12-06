5bf8a0fff9964d42875f5fa8ade6606144cf84ea
Ansh Singh <ansh04072002@gmail.com>
2022-12-06 13:47:36 +0530

    Input
    Input

--- Array/Input.java
+++ Array/Input.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         int[] arr = new int[5];
         arr[0] = 23;
         arr[1] = 45;
@@ -7,5 +10,12 @@ public class Input {
         arr[3] = 543;
         arr[4] = 3;
         System.out.println(arr[3]);
+        // input using for loops
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
