c0be7ff8c28d0fa039882db38ba359e7bc368c63
Ansh Singh <ansh04072002@gmail.com>
2022-12-16 20:05:13 +0530

    Main

--- practice/Main.java
+++ practice/Main.java
@@ -1,10 +1,15 @@
-public class Main {
-    int x = 5;
-
+class Main {
     public static void main(String[] args) {
-        Main myObj1 = new Main(); // Object 1
-        Main myObj2 = new Main(); // Object 2
-        System.out.println(myObj1.x);
-        System.out.println(myObj2.x);
+     
+      // create an array
+      int[] age = {12, 4, 5, 2, 5};
+   
+      // access each array elements
+      System.out.println("Accessing Elements of Array:");
+      System.out.println("First Element: " + age[0]);
+      System.out.println("Second Element: " + age[1]);
+      System.out.println("Third Element: " + age[2]);
+      System.out.println("Fourth Element: " + age[3]);
+      System.out.println("Fifth Element: " + age[4]);
     }
-}
+   }
