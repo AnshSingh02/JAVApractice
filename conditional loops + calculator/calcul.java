31f586158c4ebcbf0917d32a53c469c1d1ddfff0
AnshSingh02 <ansh04072002@gmail.com>
2023-02-26 09:04:48 +0530

    cal

---
+++ conditional loops + calculator/calcul.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args) {
+        Scanner in =new Scanner (System.in);
+        int ans  = 0;
+        while (true){
+            System.out.println("enter operator");
+            char op = in.next().charAt(0);
+            if (op == '+' || op =='-' || op ==' *' || op ==' /'){
+                System.out.println ("enter two numbers");
+                int num1 =in.nextInt();
+                int num2 =in.nextInt();
+                System.out.println();
+            if (op == '+'){
+                ans = num1 + num2 ;
+
+            }
+            if ( op =='-'){
+                ans = num1-num2;
+            }
+            if (op =='*'){
+                ans  =num1 *num2;
+            }
+            if ( op =='/'){
+                ans =num1 /num2;
+            }
+            
+            }
+            System.out.println(ans);
+                } 
+    }
+}
