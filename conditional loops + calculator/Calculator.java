3f7886939945aa623690dc3e6fc3706fc2b6633c
AnshSingh02 <ansh04072002@gmail.com>
2023-02-26 09:05:27 +0530

    Update Calculator.java

--- conditional loops + calculator/Calculator.java
+++ conditional loops + calculator/Calculator.java
@@ -1,35 +1,32 @@
-import java.util.Scanner ;
-public class Calculator {
-    public static void main(String[]args) {
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args) {
         Scanner in =new Scanner (System.in);
-        int ans = 0;
-        while (true) {
-            //take the operator as input
+        int ans  = 0;
+        while (true){
             System.out.println("enter operator");
-            char op = in.next().trim().charAt(0);
-            if (op == '+' || op =='-' || op == '*' || op == '%'){
-                //input two numbers 
-                System.out.println("enter two no");
+            char op = in.next().charAt(0);
+            if (op == '+' || op =='-' || op =='*' || op =='/'){
+                System.out.println ("enter two numbers");
                 int num1 =in.nextInt();
                 int num2 =in.nextInt();
                 System.out.println();
-                if (op == '+'){
-                    ans = num1 + num2;
-                }
-                if (op == '-'){
-                    ans = num1 - num2;
-                }
-                if (op == '*'){
-                    ans = num1 * num2;
-                } if (op == '%'){
-                    ans = num1 % num2;
-                
-                }
-            }
-        
-            System.out.println(ans);
-        
-    }
+            if (op == '+'){
+                ans = num1 + num2 ;
 
+            }
+            if ( op =='-'){
+                ans = num1-num2;
+            }
+            if (op =='*'){
+                ans  =num1 *num2;
+            }
+            if ( op =='/'){
+                ans =num1 /num2;
+            }
+            
+            }
+            System.out.println(ans);
+                } 
     }
 }
