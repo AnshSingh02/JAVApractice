399b14d46d64a1e6fce49683115ac3e414a8512c
AnshSingh02 <ansh04072002@gmail.com>
2023-03-09 22:19:48 +0530

    Update Swap.java

--- Array/Swap.java
+++ Array/Swap.java
@@ -6,6 +6,8 @@ public class Swap {
         swap(arr ,1 ,3);
     System.out.println(Arrays.toString(arr));
     }
+
+
     static void swap(int[] arr,int index1, int index2){
         int temp = arr[index1]; 
         arr[index1] = arr[index2];
