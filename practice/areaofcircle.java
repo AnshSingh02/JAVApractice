dbbdec8c398deb457bf1707c696e9feb726a5607
AnshSingh02 <ansh04072002@gmail.com>
2023-03-28 22:40:07 +0530

    Create areaofcircle.java

---
+++ practice/areaofcircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class areaofcircle{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter radius");
+        int r=nextInt();
+        area = 3.14*r*r;
+        System.out.println(area);
+    }
+}
