56ac9d87675a3d4ec31e12074e67027ad6bb4fe7
AnshSingh02 <ansh04072002@gmail.com>
2023-04-03 08:01:04 +0530

    Update JavaBasics.java

--- practice/JavaBasics.java
+++ practice/JavaBasics.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
+import java.util.*;
 public class JavaBasics {
-    public static void main (String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int a =sc.nextInt();
-        int b =sc.nextInt();
-        int sum = a + b;
-        System.out.println(sum);
-       
+    public static void main(String[] args) {
+        int number = 2;
+     switch(number) {
+        case 1 :System.out.println("samosa");
+        break;
+        case 2 :System.out.println("roll");
+        break;
+        case 3 :System.out.println("momos");
+        break;
+        default :System.out.println("idli");
+
+     }
     }
- }
+}
