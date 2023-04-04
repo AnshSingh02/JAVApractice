1f3025b620ed2a3218e133773887a8906aa0fc33
AnshSingh02 <ansh04072002@gmail.com>
2023-04-04 22:28:19 +0530

    Update JavaBasics.java

--- practice/JavaBasics.java
+++ practice/JavaBasics.java
@@ -3,6 +3,9 @@ public class JavaBasics {
     public static void main(String[] args) {
         int number = 2;
      switch(number) {
+
+
+        
         case 1 :System.out.println("samosa");
         break;
         case 2 :System.out.println("roll");
