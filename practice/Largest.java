0ceb0962878dfb95c0c35be2bdad31dcc8ff30b2
AnshSingh02 <ansh04072002@gmail.com>
2023-04-24 08:40:57 +0530

    Update Largest.java

--- practice/Largest.java
+++ practice/Largest.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 public class Largest{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a=in.nextInt();
-        int b=in.nextInt();
-        int c=in.nextInt();
-
-        if((a>=b)&&(a>=c)){
-            System.out.println("a is largest");
+        Scanner in =new Scanner(System.in);
+        System.out.println("enter two nembers :");
+        int a =in.nextInt();
+        int b =in.nextInt();
+        
+        if (a>=b){
+            System.out.println(" a is largest");
+            
         }
-        else if(b>=c){
+        else{
             System.out.println("b is largest");
-
-        }
-        else {
-            System.out.println("c is largest");
-
-
+            
         }
     }
 }
