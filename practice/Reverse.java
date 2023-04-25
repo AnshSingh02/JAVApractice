44ee89237f48d39a18e9d693eafe32dab72dde4d
AnshSingh02 <ansh04072002@gmail.com>
2023-04-25 09:31:35 +0530

    Create Reverse.java

---
+++ practice/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reverse{
+    public static void main(String[] args) {
+       
+         int n = 10899;
+
+         while (n > 0){
+            int lastDigit =n% 10;
+            System.out.println(lastDigit);
+            n = n / 10;
+         }
+         System.out.println();
+    }
+}
