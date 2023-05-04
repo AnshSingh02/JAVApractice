ce936d667bc89c8db0ae760c9b2fc539a079e24c
AnshSingh02 <ansh04072002@gmail.com>
2023-05-04 22:11:30 +0530

    Update pyramid.java

--- practice/pyramid.java
+++ practice/pyramid.java
@@ -3,6 +3,8 @@ public class pyramid{
     public static void main(String[] args) {
         int n = 4 ;
         for (int line=1; line<=n;line++){
+
+            
             for(int number=1; number<=line; number++){
                 System.out.print(number);
             }
