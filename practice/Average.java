d9dd237e79e9bc724a2e72b54957cd85580defef
AnshSingh02 <ansh04072002@gmail.com>
2023-05-08 22:54:59 +0530

    Update Average.java

--- practice/Average.java
+++ practice/Average.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-public  class Average{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int c = sc.nextInt();
-   
-    int avg = (a + b + c) / 3;
-    System.out.println(avg);
-}
+public class Average{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter 3 numbers");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+     int avg = (a+b+c) /3;
+     System.out.println(avg);
+     
+    }
 }
