778fb19390c9b82240a7a0d417f17e3367aca42d
AnshSingh02 <ansh04072002@gmail.com>
2023-05-12 22:49:19 +0530

    Create invertedpyramid.java

---
+++ conditional loops + calculator/invertedpyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class invertedpyramid{
+    public static void inverted_inverted_rotated_halfpyramid(int n){
+        for(int i=1 ;i<=n ;i++){
+            for(int j =1 ;j <=n-i ; j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+     public static void main(String[] args) {
+            inverted_inverted_rotated_halfpyramid(4);
+        }
+
+}
