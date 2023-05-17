4df75e09fb5ac0a8c516083a965438dcd9dde86f
AnshSingh02 <ansh04072002@gmail.com>
2023-05-17 08:30:50 +0530

    Create JavaBasics.java

---
+++ Functions,Methods/JavaBasics.java
@@ -0,0 +1,13 @@
+import java.util.
+public class JavaBasics{
+
+    public static void printHelloWorld() {
+
+        System.out.println("hello world");
+    }
+
+    public static void main(String args[]) {
+        Scanner sc =new Scanner(System.in);
+
+    }
+}
