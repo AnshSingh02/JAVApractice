a13cb51ca2b6904225c98f1082b72a8329e70e91
AnshSingh02 <ansh04072002@gmail.com>
2023-05-19 23:23:12 +0530

    Update ArrayCC.java

--- Array/ArrayCC.java
+++ Array/ArrayCC.java
@@ -1,16 +1,20 @@
 import java.util.*;
 
 public class ArrayCC {
-    public static void update(int marks[]){
+    public static void update(int marks[],int nonChangable){
+        nonChangable = 10; 
         for(int i =0;i<marks.length;i++){
             marks[i]=marks[i]+1;
         }
     }
     public static void main(String[] args) {
         int marks[] = {97,98,99};
+        int nonChangable = 5;
 
-       update(marks);
+       update(marks,nonChangable);
+       System.out.println(nonChangable);
 
+        //print our marks
        for(int i=0 ; i<marks.length ; i++){
         System.out.print(marks[i]+" ");
        }
