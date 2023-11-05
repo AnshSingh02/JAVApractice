c126181b21d40e07cf2da9a29f1ce08bcf0129cb
AnshSingh02 <ansh04072002@gmail.com>
2023-11-05 11:48:11 +0530

    Create hollow square.java

---
+++ practice/hollow square.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hollowsquare{
+    public static void main(String []rgs){
+        int n = 5;
+        for(int  i = 0 ; i<n ;i++){
+            for (int j = 0 ; j <n ; j ++){
+                if (i == 0 || j == 0 || i == 4 || j == 4){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
