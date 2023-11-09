33c838dc5df4399cfd8556087f0664b2aed59397
AnshSingh02 <ansh04072002@gmail.com>
2023-11-09 15:36:12 +0530

    h

---
+++ practice/Diagonal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Diagonal {
+  public  static void main(String [] args){
+    int n = 6 ;
+    for (int i =0 ;i<n ; i++){
+        for (int j =0; j<n ; j++){
+            if(i+j == (n-1) || i == j){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println(" ");
+    }
+  }
+}
--- practice/LetterD.java
+++ practice/LetterD.java
@@ -4,7 +4,7 @@ public class LetterD{
         int n = 10;
         for (int i = 0 ; i<n ; i++){
             for (int j = 0 ;j<n ; j++){
-                if (i==0 && j<(n-1)/2 || i== n-1 && j<(n-1)/2 ||j ==0 || j==(n-1)/2 && i>0 &&i<n-1 ){
+                if (i==0 && j<(n-1)/2 || i== n-1 && j<(n-1)/2 ||j ==0 || j==(n-1)/2 && i>0 &&i<n-1  ){
                     System.out.print("*");
                 }
                 else{
